1041d8f46a2669305f79e868f5a2edd2ba66d1c5
chaitak-gorai <chaitakgorai@gmail.com>
2022-01-19 19:02:38 +0530

    ⚡️ Loader updated

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -5,7 +5,7 @@ import { Loader } from '.';
 const companyCommonStyles =
   'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white';
 
-const Input = (placeholder, name, type, value, handleChange) => (
+const Input = ({ placeholder, name, type, value, handleChange }) => (
   <input
     placeholder={placeholder}
     type={type}
@@ -21,7 +21,7 @@ const Welcome = () => {
   const handleSubmit = () => {};
   return (
     <div className='w-full flex justify-center items-center '>
-      <div className='flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4'>
+      <div className='flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4'>
         <div className='flex flex-1 justify-start items-start flex-col mf:mr-10'>
           <h1 className='text-3xl sm:text-5xl text-white text-gradient py-1'>
             Send Crypto <br /> across the world
@@ -55,7 +55,7 @@ const Welcome = () => {
           </div>
         </div>
 
-        <div className='flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10'>
+        <div className='flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10'>
           <div className='p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism '>
             <div className='flex justify-between flex-col w-full h-full'>
               <div className='flex justify-between items-start'>
