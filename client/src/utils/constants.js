331e6386b6f96271743df83830deea1508206a39
itisdb <dhriteshbhagat@gmail.com>
2022-01-26 01:40:43 +0530

    Update code

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,3 +1,3 @@
 import abi from './Transactions.json';
 export const contractABI = abi.abi;
-export const contractAddress = 'DEPLOYED CONTRACT ADDRESS';
+export const contractAddress = '0x6C4FaFA6C395C2B89644645c4171852dDC51074c';
